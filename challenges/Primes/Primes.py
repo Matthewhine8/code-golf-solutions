42085a6dfc9c5264035ca29a6af1782a6607ec82
Matthewhine8 <106594095+Matthewhine8@users.noreply.github.com>
2025-08-26 11:33:55 +0100

    Update Primes.py
    Reduced number of characters by removing second if statement

--- challenges/Primes/Primes.py
+++ challenges/Primes/Primes.py
@@ -1,5 +1,3 @@
 for i in range(2,100):
-  if i==2 or i==3 or i==5 or i==7:
-    print(i)
-  if i%2!=0 and i%3!=0 and i%5!=0 and i%7!=0:
+  if i==2 or i==3 or i==5 or i==7 or (i%2!=0 and i%3!=0 and i%5!=0 and i%7!=0):
     print(i)
